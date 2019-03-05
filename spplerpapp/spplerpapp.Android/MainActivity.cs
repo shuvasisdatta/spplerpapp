a199c7645574fd47806a71590daf5ee14ffe0691
shuvasisdatta <shuvasisdatta@gmail.com>
2019-03-05 15:59:48 +0600

    andriod app - web version

--- spplerpapp/spplerpapp.Android/MainActivity.cs
+++ spplerpapp/spplerpapp.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace spplerpapp.Droid
 {
-    [Activity(Label = "spplerpapp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "SPPL ERP", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -20,6 +20,17 @@ namespace spplerpapp.Droid
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+        }
+
+        // system back button
+        public override bool OnKeyDown(Keycode keyCode, KeyEvent e)
+        {
+            if (keyCode == Keycode.Back)
+            {
+                System.Environment.Exit(0);
+                return true;
+            }
+            return base.OnKeyDown(keyCode, e);
         }
     }
 }
--- spplerpapp/spplerpapp/MainPage.xaml.cs
+++ spplerpapp/spplerpapp/MainPage.xaml.cs
@@ -13,5 +13,33 @@ namespace spplerpapp
         {
             InitializeComponent();
         }
+            
+        public void OnBackButtonClicked(object sender, EventArgs e)
+        {
+            if (webView.CanGoBack)
+            {
+                webView.GoBack();
+            }
+            else
+            {
+                System.Environment.Exit(0); // Exit from App
+            }
+        }
+        
+        void webviewNavigating(object sender, WebNavigatingEventArgs e)
+        {
+            webView.IsVisible       = false;
+            labelLoading.IsVisible  = true;
+            indicator.IsRunning     = true;
+            indicator.IsVisible     = true;
+        }
+
+        void webviewNavigated(object sender, WebNavigatedEventArgs e)
+        {
+            labelLoading.IsVisible  = false;
+            indicator.IsRunning     = false;
+            indicator.IsVisible     = false;
+            webView.IsVisible = true;
+        }
     }
 }
